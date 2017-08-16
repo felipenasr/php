90e935a888680f91742a311d25a85736993bd90f
felipenasr <felipenriccitrab@gmail.com>
2017-08-16 16:22:27 -0300

    estruturas condicionais

--- estruturas_condicionais/exemplo-01.php
+++ estruturas_condicionais/exemplo-01.php
@@ -1,2 +1,20 @@
-<?php 
+<?php
+
+	$suaIdade = 69;
+
+
+	$idadeCrianca = 12;
+	$idadeAdulto = 18;
+	$idadeIdoso = 65;
+
+	if($suaIdade < $idadeCrianca){
+		echo "Criança";
+	}elseif($suaIdade < $idadeAdulto){
+		echo "Adolescente";
+	}elseif($suaIdade < $idadeIdoso){
+		echo "Você é adulto";
+	}else{
+		echo "Você é idoso";
+	}
+
 ?>
