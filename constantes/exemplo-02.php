49ca296e5875f69826c2745725a190097f9be985
felipenasr <felipenriccitrab@gmail.com>
2017-08-17 14:42:59 -0300

    sessoes e funcoes

---
+++ constantes/exemplo-02.php
@@ -0,0 +1,11 @@
+<?php 
+	define("BANCO_DE_DADOS", [
+		'127.0.0.1',
+		'root',
+		'passwd',
+		'teste'
+		]);
+
+	print_r(BANCO_DE_DADOS);
+
+?>
---
+++ session/exemplo-05.php
@@ -0,0 +1,6 @@
+<?php 
+	require_once("config.php");
+
+	echo session_save_path();
+	var_dump(session_status());
+?>
