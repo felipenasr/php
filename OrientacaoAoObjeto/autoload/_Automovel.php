d57eb580e5dc53aad34d3851f8018c03c9cbe9e2
felipenasr <felipenriccitrab@gmail.com>
2017-08-31 10:44:22 -0300

    polimorfismo e autoload

---
+++ OrientacaoAoObjeto/autoload/_Automovel.php
@@ -0,0 +1,23 @@
+<?php
+	interface Veiculo{
+
+		public function acelerar($velocidade);
+		public function freiar($velocidade);
+		public function trocarMarcha($marcha);
+
+	}
+
+	abstract class Automovel implements Veiculo{
+		
+		public function acelerar($velocidade){
+			echo "o Veiculo acelerou ".$velocidade;
+		}
+		public function freiar($velocidade){
+			echo "o Veiculo freiou ".$velocidade;
+		}
+		public function trocarMarcha($marcha){
+			echo "o Veiculo trocou para a marcha ".$marcha;
+		}
+	
+	}
+?>
